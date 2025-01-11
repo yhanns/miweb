23d9a4ddbd323a3d0f28a576083cdb6ae2483875
yhanns <Yhannssalerno@gmail.com>
2025-01-11 15:39:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def get_piezas():
 
 # Ejecutar el servidor solo si este archivo se ejecuta directamente
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
